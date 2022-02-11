03f4ac2ef0cb72e8814e1b92b85daa7ccd62a89e
motosw3600 <motosw3600@gmail.com>
2022-02-11 19:23:07 +0900

    layout: TextField, Label Create

--- TextField Update/TextField Update/ViewController.swift
+++ TextField Update/TextField Update/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

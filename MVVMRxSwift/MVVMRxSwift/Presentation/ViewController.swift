ce33973346dd67eba965754d61b45e93852bb1f3
motosw3600 <motosw3600@gmail.com>
2022-03-07 17:41:42 +0900

    feat: bind RxTableView

--- MVVMRxSwift/MVVMRxSwift/Presentation/ViewController.swift
+++ MVVMRxSwift/MVVMRxSwift/Presentation/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    private var viewModel: MenuListViewModel?
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.bind()
+    }
+     
+    static func instantiate(viewModel: MenuListViewModel) -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        viewController.viewModel = viewModel
+        return viewController
+    }
+    
+    private func bind() {
+        viewModel?.fetchMenuViewModels().bind(to:
+                                                tableView.rx.items(cellIdentifier: "cell")) { index, viewModel, cell in
+            cell.textLabel?.text = viewModel.displayText
+        }.disposed(by: disposeBag)
     }
 
 
